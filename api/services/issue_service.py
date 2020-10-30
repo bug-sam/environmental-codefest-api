cd125b5b78249cb3c590b35ff260b16e13fc85de
Maciej Lewicki <lewickimapl@gmail.com>
2020-10-30 18:54:53 +0100

    filled in issue_service, still need to hook up database properly

--- api/services/issue_service.py
+++ api/services/issue_service.py
@@ -2,13 +2,14 @@ from api.app import db
 from api.models.issue import Issue
 
 
-def save_new_issue(issue):
-    pass
+def save_new_issue(title, preview, message, sources):
+    issue = Issue(title=title, preview=preview, message=message, sources=sources)
+    db.session.add(issue)
+    db.session.commit()
 
 
-def get_all_users():
-    pass
+def get_all_issues():
+    return Issue.query.all()
 
-
-def get_a_user(public_id):
-    pass
+def get_issue_by_id(public_id):
+    return Issue.query.filter_by(id=public_id).first()
